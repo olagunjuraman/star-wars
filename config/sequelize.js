b2632ad202d48409ce308ba6db1ce09b1be1eebd
olagunjuraman <olagunjuraman@gmail.com>
2021-11-20 22:47:53 +0100

    added-proc-file

--- config/sequelize.js
+++ config/sequelize.js
@@ -6,7 +6,7 @@ const databaseUrls = {
   development: process.env.DATABASE_URL,
   staging: process.env.DATABASE_URL,
   test: process.env.DATABASE_URL,
-  production: process.env.DATABSE_URL,
+  production: process.env.DATABASE_URL,
 };
 
 const environment = process.env.NODE_ENV || 'development';
